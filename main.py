ea7d7fd67ec3d7987d681580272b0550d214e066
Muhammed Musthafa Shahal V <musthuvakkayil@gmail.com>
2022-04-19 16:17:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from right_side import right
 from left_side import left
 
-rc = right()
-lc = left()
+#rc = right()
+#lc = left()
 
 import time
 def sleep(seconds):
@@ -11,53 +11,66 @@ def sleep(seconds):
     print("done")
 #rc=3
 #lc=2
-#while(true):
+current=1
+def test():
+    global current
 
-flagr=0
-flagl=0
-if(rc>lc):
-    current=1
-    flagr=flagr+1
-    flagl=0
-    if(flagr==3 or (flagr==2 and rc-lc<=2)):
-        current=0
-        flagl=flagl+1
-        flagr=0
-        timer=45+(3*(lc-1))
-        print("green @ left")
-        sleep(timer)
-    timer=45+(3*(rc-1))
-    print("green @ right")
-    sleep(timer)
-elif(lc>rc):
-    current=0
-    flagl=flagl+1
+    rc = right()
+    lc = left()
+
     flagr=0
-    if (flagl==3 or (flagl==2 and lc-rc <= 2)):
+    flagl=0
+    if(rc>lc):
         current=1
         flagr=flagr+1
         flagl=0
+        if(flagr==3 or (flagr==2 and rc-lc<=2)):
+            current=0
+            flagl=flagl+1
+            flagr=0
+            timer=45+(3*(lc-1))
+            print("green @ left")
+            sleep(timer)
+            test()
         timer=45+(3*(rc-1))
         print("green @ right")
         sleep(timer)
-    timer =45+(3*(lc-1))
-    print("green @ left")
-    sleep(timer)
-else:
-    if(current==1):
-        current = 0
+        test()
+    elif(lc>rc):
+        current=0
         flagl=flagl+1
         flagr=0
-        timer=45+(3*(lc-1))
+        if (flagl==3 or (flagl==2 and lc-rc <= 2)):
+            current=1
+            flagr=flagr+1
+            flagl=0
+            timer=45+(3*(rc-1))
+            print("green @ right")
+            sleep(timer)
+            test()
+        timer =45+(3*(lc-1))
         print("green @ left")
+        sleep(timer)
+        test()
     else:
-        current = 1
-        flagr=flagr+1
-        flagl = 0
-        timer = 45 + (3 * (rc - 1))
-        print("green @ right")
+        if(current==1):
+            current = 0
+            flagl=flagl+1
+            flagr=0
+            timer=45+(3*(lc-1))
+            print("green @ left")
+            sleep(timer)
+            test()
+        else:
+            current = 1
+            flagr=flagr+1
+            flagl = 0
+            timer = 45 + (3 * (rc - 1))
+            print("green @ right")
+            sleep(timer)
+            test()
 
-
+test()
 
 
 
